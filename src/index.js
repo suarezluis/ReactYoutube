8962ddc756644630859808bf7e7f362a2973d28e
Luis Suarez <suarezluis@gmail.com>
2018-08-22 22:09:03 -0500

    Throttle down search so results wont update with every keystroke

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
+import _ from "lodash";
 
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
@@ -28,9 +29,14 @@ class App extends Component {
   }
 
   render() {
+    // Throttle down search so results wont update with every keystroke
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
+
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <SearchBar onSearchTermChange={term => videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
