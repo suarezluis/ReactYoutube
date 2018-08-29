d5061448434398ad9e11b1f54613b5b3dc6e1150
Luis Suarez <suarezluis@gmail.com>
2018-08-28 21:32:40 -0500

    switched to create-react-app file structure

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -10,7 +10,12 @@ const VideoDetail = ({ video }) => {
   return (
     <div className="video-detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
-        <iframe src={url} frameBorder="0" className="embed-responsive-item" />
+        <iframe
+          title="Video"
+          src={url}
+          frameBorder="0"
+          className="embed-responsive-item"
+        />
       </div>
       <div className="details">
         <div>{video.snippet.title}</div>
